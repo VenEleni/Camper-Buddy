dfe55186070758ed541214f1ddaedf06739ef3f1
Eleni Veniou Nikolidaki <venelenii@gmail.com>
2024-07-28 15:39:32 +0300

    add forum and order schema model

---
+++ server/models/forumModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const ForumSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  comments: [CommentSchema],
+});
+
+const Forum = mongoose.model("Forum", ForumSchema);
+const Comment = mongoose.model("Comment", CommentSchema);
+
+module.exports = {Forum, Comment};
---
+++ server/models/orderSchema.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const OrderSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  orderDate: {
+    type: Date,
+    default: Date.now,
+  },
+  products: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  address: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["pending", "shipped", "delivered"],
+    default: "pending",
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
+  total: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const Order = mongoose.model("Order", OrderSchema);
+
+module.exports = Order;
