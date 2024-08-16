b50d28617a10dc9958b1de99fb5bca58d5c3f0cc
Eleni Veniou Nikolidaki <venelenii@gmail.com>
2024-08-16 13:46:10 +0300

    add removeFromCart logic

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -2,14 +2,16 @@ import React, { useEffect, useState } from "react";
 import EshopNavBar from "../components/EshopNavBar";
 import { useDispatch, useSelector } from "react-redux";
 // import axiosInstance from "../components/axiosInstance";
-import {fetchCartItems} from "../actions/cartActions";
+import { fetchCartItems } from "../actions/cartActions";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { removeFromCart } from "../actions/cartActions";
 
 const FetchCart = () => {
   const dispatch = useDispatch();
   const fetchCart = useSelector((state) => state.fetchCart);
-  // const auth = JSON.parse(localStorage.getItem('auth'));
-  // const userId = auth && auth.user ? auth.user.id : null;
-  // const token = auth.token;
+  const auth = JSON.parse(localStorage.getItem("auth"));
+  const userId = auth && auth.user ? auth.user.id : null;
+  const token = auth.token;
   const { loading, error, cartItems } = fetchCart || [];
 
   // console.log("userId in FetchCart: ", userId);
@@ -20,20 +22,46 @@ const FetchCart = () => {
     dispatch(fetchCartItems());
   }, []);
 
+  const handleRemoveFromCart = async (product) => {
+    if (userId) {
+      console.log(
+        "I'm in the handleRemoveFromCart and product is :",
+        product._id
+      );
+      try {
+        await dispatch(removeFromCart(userId, product._id));
+        console.log("Product removed from cart successfully");
+        window.location.reload();
+      } catch (error) {
+        console.error("Error removing product from cart:", error);
+      }
+    } else {
+      console.log("User not authenticated for deleting from cart");
+    }
+  };
+
   return (
     <>
-    <div>
-    <EshopNavBar />
-    </div>
-      
+      <div>
+        <EshopNavBar />
+      </div>
+
       <div className="bg-red-300 top-36">
-        {(cartItems && cartItems.length > 0) ? (
+        {cartItems && cartItems.length > 0 ? (
           cartItems.map((item) => (
-            <div key={item.product._id}>
-              <img className="w-24" src={item.product.image} alt={item.product.title} />
+            <div key={item.product._id} className="flex">
+              <img
+                className="w-24"
+                src={item.product.image}
+                alt={item.product.title}
+              />
               <p>{item.product.title}</p>
               <p>{item.product.price} €</p>
               <p>Quantity: {item.quantity}</p>
+              <button
+                className="bi bi-x-lg"
+                onClick={() => handleRemoveFromCart(item.product)}
+              ></button>
             </div>
           ))
         ) : (
