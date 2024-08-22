cc179e6ebc13114851a5ebd97223d3a67ceb07b4
Eleni Veniou Nikolidaki <venelenii@gmail.com>
2024-08-22 19:55:15 +0300

    add styling and condition rendering on product details

--- client/src/pages/ProductDetails.jsx
+++ client/src/pages/ProductDetails.jsx
@@ -4,6 +4,7 @@ import { addToCart } from "../actions/cartActions";
 import './ProductDetails.css';
 import {createReview} from '../actions/reviewActions';
 import {fetchReviews} from '../actions/reviewActions';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const ProductDetails = ({ product, onBack }) => {
     const dispatch = useDispatch();
@@ -12,10 +13,12 @@ const ProductDetails = ({ product, onBack }) => {
     const [totalStars, setTotalStars] = useState(5);
     const [comment, setComment] = useState("");
     const [averageRating, setAverageRating] = useState(0);
+    const auth = useSelector((state) => state.auth);
+    const [cartMessage, setCartMessage] = useState(""); 
+    const [submitReviewMessage, setSubmitReviewMessage] = useState(""); 
 
 
-  const auth = JSON.parse(localStorage.getItem('auth'));
-  const token = auth.token;
+  // const auth = JSON.parse(localStorage.getItem('auth'));
 
   useEffect(() => {
     dispatch(fetchReviews(product._id));
@@ -34,6 +37,8 @@ const ProductDetails = ({ product, onBack }) => {
     }
   }, [product.reviews]);
 
+
+
 // const handleChange = (e) => {
 //   console.log("e.target.value is : ", e.target.value);
 //   const value = parseInt(e.target.value, 10);
@@ -50,11 +55,13 @@ const ProductDetails = ({ product, onBack }) => {
     if (userId){
       try {
         await dispatch(addToCart(userId, product._id));
-        console.log("Product added to cart successfully");
+        setCartMessage("Product added to cart successfully");
       } catch (error) {
+        setCartMessage("Error adding product to cart!");
         console.error("Error adding product to cart:", error);
       }
     } else {
+      setCartMessage("User not authenticated");
       console.log("User not authenticated");
     }
   };
@@ -66,43 +73,55 @@ const ProductDetails = ({ product, onBack }) => {
     
     try {
         await dispatch(createReview(product._id, reviewData));
+        setSubmitReviewMessage("Review submitted successfully");
         console.log("Review submitted successfully:", reviewData);
     } catch (error) {
+        setSubmitReviewMessage("Error submitting review!");
         console.error("Error submitting review:", error);
     }
 };
 
   return (
     <>
-    <div className="grid grid-cols-2 top-20">
+    <i className="bi bi-arrow-left text-black cursor-pointer mt-3 ml-20" onClick={onBack}></i>
+    <div className="grid grid-cols-2">
+    
       <img className="w-96 col-span-6 ml-40 " src={product.image} alt={product.name} />
-      <div className="col-span-6 ">
-        <h2>{product.title}</h2>
+      <div className="col-span-6 mr-10">
+        <h2 className="text-black">{product.title}</h2>
 
-        <p>{product.description}</p>
-        <p>{product.price} €</p>
-        <p>Average Rating: {averageRating.toFixed(0)} / 5</p>
-        <button onClick={onBack}>Go Back</button>
+        <p className="text-black">{product.description}</p>
+        <p className="text-black">{product.price} €</p>
+        <p className="text-black">Rating: {averageRating.toFixed(0)} / 5</p>
+        {auth.isAuthenticated ? (
+          <>
         <button onClick={handleAddToCart} >Add to cart </button>
+        {cartMessage && <p className="text-black mt-2">{cartMessage}</p>}
+        </>
+        ) : (
+          <p>Please <a href="/login">log in</a> to add to cart</p>
+        )}
       </div>
     </div>
-    <div className="mt-64 ml-96 mb-64">
-      <div>
-    <h3>Reviews</h3>
+    <div className="mt-10  mb-64 pb-8">
+      <div >
+    <h3 className="text-black ml-10">Reviews:</h3>
+    {product.reviews && product.reviews.length === 0 && <p className="text-black ml-10">No reviews yet</p>}
     <ul>
       {product.reviews && product.reviews.map((review) => (
-        <li key={review._id}>
+        <li className="text-black" key={review._id}>
            <p><strong>{review.username}</strong> says:</p>
-          <p>Rating: {review.rating}</p>
+          <p className="text-black">Rating: {review.rating}</p>
           <p>Comment: {review.comment}</p>
         </li>
       ))}
     </ul>
     </div>
-      <form onSubmit={handleSubmit}>
+    {auth.isAuthenticated ? (
+      <form className="bg-white ml-96 left-40 w-96 h-96" onSubmit={handleSubmit}>
     <fieldset >
   <div className="rating">
-      <h5>Star rating</h5>
+      <h5 className="text-black">Star rating</h5>
       {[...Array(totalStars)].map((star, index) => {
         const currentRating = index + 1;
 
@@ -116,11 +135,7 @@ const ProductDetails = ({ product, onBack }) => {
               onChange={() => {setRating(currentRating)}}
             />
             <span
-              className="star"
-              style={{
-                color:
-                  currentRating <= (hover || rating) ? "#ffc107" : "#e4e5e9",
-              }}
+              className={`star ${currentRating <= (hover || rating) ? 'star-highlight' : 'star-default'}`}
               onMouseEnter={() => setHover(currentRating)}
               onMouseLeave={() => setHover(null)}
             >
@@ -129,17 +144,21 @@ const ProductDetails = ({ product, onBack }) => {
           </label>
         );
       })}
-      <p>Your rating is: {rating}</p>
+      <p className="text-black">Your rating is: {rating}</p>
       <br />
     </div>
   <div className="field border label textarea">
-    <textarea value={comment} onChange={(e) => setComment(e.target.value)}></textarea>
+    <textarea className="text-black review_textarea" value={comment} onChange={(e) => setComment(e.target.value)}></textarea>
     
-    <span className="helper">Write your review</span>
+    <span className="helper text-black">Write your review</span>
   </div>
 </fieldset>
 <button type="submit">Submit</button>
+{submitReviewMessage && <p className="text-black mt-2">{submitReviewMessage}</p>}
 </form>
+    ): (
+      <p className="text-black">Please<a href="/login">log in</a> to leave a review</p>
+    )}
 </div>
 
 
